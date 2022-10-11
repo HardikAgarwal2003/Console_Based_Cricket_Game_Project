34f62ed57f83d3f83b724a3c0a6d6400adbe7cf6
Hardik Agarwal <113618138+HardikAgarwal2003@users.noreply.github.com>
2022-10-11 21:22:44 +0530

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,13 @@
+#include<string>
+
+class Player {
+	public:
+		Player();	//Default Constructor...
+		std::string name;
+		int id;
+		int runsScored;
+		int ballsPlayed;
+		int ballsBowled;
+		int runsGiven;
+		int wicketsTaken;
+};
